020497a3c963e1628cc107953cc6e6936931c101
jever <1103349641@qq.com>
2019-04-25 10:53:27 +0800

    StackNavigator finish

--- navigators/AppNavigators.js
+++ navigators/AppNavigators.js
@@ -7,24 +7,57 @@ import {createStackNavigator, createAppContainer} from 'react-navigation'
 import HomePage from '../pages/HomePage'
 import Page1 from '../pages/Page1'
 import Page2 from '../pages/Page2'
+import Page3 from '../pages/Page3'
+import React from 'react'
+import {Button} from 'react-native'
+
 
 const AppStackNavigator = createStackNavigator({
     HomePage: {
         screen: HomePage,
-        navigationOptions: {
-            header: null
-        }
     },
     Page1: {
-        screen: Page1
+        screen: Page1,
+        navigationOptions: ({navigation}) => ({
+            title: `${navigation.state.params.name}页面`
+        })
     },
     Page2: {
-        screen: Page2
+        screen: Page2,
+        navigationOptions: {
+            title: 'Page2',
+            headerStyle: {
+                backgroundColor: '#f4511e',
+            },
+            headerTintColor: '#fff',
+            headerTitleStyle: {
+                fontWeight: 'bold',
+            },
+        }
+    },
+    Page3: {
+        screen: Page3,
+        navigationOptions: (props) => {
+            const {navigation} = props;
+            const {state, setParams} = navigation;
+            const {params} = state;
+            return {
+                title:params.title?params.title:'This is Page3',
+                headerRight: (
+                    <Button
+                        title={params.mode === 'edit'? '保存': '编辑'}
+                        onPress={() => {
+                            setParams({mode:params.mode === 'edit'? '':'edit'})
+                        }}
+                    />
+                )
+            }
+        }
     }
 }, {
-    defaultNavigationOptions: {
-        header: null
-    }
+    // defaultNavigationOptions: {
+    //     header: null
+    // }
 })
 
 export default createAppContainer(AppStackNavigator)
--- pages/Page3.js
+++ pages/Page3.js
@@ -1,48 +1,50 @@
 /**
  * @author: jever
  * @date: 2019/4/24
- * @description:  HomePage.js
+ * @description:  Page1.js
  */
 import React, {Component} from 'react';
-import {Platform, StyleSheet, Text, View, Button} from 'react-native';
+import {View, Text, StyleSheet,Button,TextInput} from 'react-native';
 
-
-export default class HomePage extends Component {
-    static navigationOptions = {
-        title: "Home",
-        headerBackTitle:'返回哈哈'
-    }
+export default class Page3 extends Component {
     render() {
         const {navigation} = this.props;
+        console.log(navigation)
+        const {state, setParams} = navigation;
+        const {params} = state;
+        const showText = params.mode === 'edit'?'正在编辑':'编辑完成';
         return (
             <View style={styles.container}>
                 <Text style={styles.welcome}>
-                    Welcome to React Native!
+                    欢迎来到Page2
                 </Text>
                 <Button
-                    title={"Go to page1"}
+                    title={'Go Back'}
                     onPress={() => {
-                        navigation.navigate('Page1', {name: '动态的'})
+                        navigation.goBack();
                     }}
                 />
-                <Button
-                    title={"Go to page2"}
-                    onPress={() => {
-                        navigation.navigate('Page2')
-                    }}
-                />
-                <Button
-                    title={"Go to page3"}
-                    onPress={() => {
-                        navigation.navigate('Page3', {title:'title Page 3'})
+                <Text>{showText}</Text>
+                <TextInput
+                    style={styles.input}
+                    onChangeText={text => {
+                        setParams({title: text})
                     }}
                 />
             </View>
-        );
+        )
     }
 }
 
+
 const styles = StyleSheet.create({
+    input: {
+        width:360,
+        height:50,
+        borderWidth:1,
+        marginTop:20,
+        borderColor:'black'
+    },
     container: {
         flex: 1,
         justifyContent: 'center',
